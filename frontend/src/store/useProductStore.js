cdd5ad2b0f8a1571524a88cd67e55a9b0976d3aa
mjnportfolio <mjnportfolio@proton.me>
2025-10-16 15:42:47 +0800

    7 - Fetch Products in Home Page

---
+++ frontend/src/store/useProductStore.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+
+export const useProductStore = create((set, get) => ({
+  products: [],
+  loading: false,
+  error: null,
+
+  fetchProducts: async () => {
+    set({ loading: true });
+    try {
+      const response = await axios.get(`${BASE_URL}/api/products`);
+      set({ products: response.data.data, error: null });
+    } catch (err) {
+      set({ error: "Something went wrong", products: [] });
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
